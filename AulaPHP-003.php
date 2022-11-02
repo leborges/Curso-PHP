44f956392151d5dfa5e9c9deb2804b73352e6b83
leborges <68827770+leborges@users.noreply.github.com>
2022-11-02 15:01:52 -0300

    Aulas add

--- AulaPHP-003.php
+++ AulaPHP-003.php
@@ -13,26 +13,133 @@
 	<div id="formulario">
 		<form method="get" autocomplete="off">
 			<fieldset>
+				<legend>Aula - Funções String</legend>
+				<fieldset>
+					<legend>StringToLower</legend>
+					<label for="cSTL">Texto:</label>
+					<input type="text" name="tSTL" id="cSTL">
+				</fieldset>
+				<fieldset>
+					<legend>StringToUpper</legend>
+					<label for="cSTU">Texto:</label>
+					<input type="text" name="tSTU" id="cSTU">
+				</fieldset>
+				<fieldset>
+					<legend>UC First</legend>
+					<label for="cUCF">Texto:</label>
+					<input type="text" name="tUCF" id="cUCF">
+				</fieldset>
+				<fieldset>
+					<legend>UC Words</legend>
+					<label for="cUCW">Texto:</label>
+					<input type="text" name="tUCW" id="cUCW">
+				</fieldset>
+				<fieldset>
+					<legend>String Reverse</legend>
+					<label for="cStrRev">Texto:</label>
+					<input type="text" name="tStrRev" id="cStrRev">
+				</fieldset>
+				<fieldset>
+					<legend>String POS</legend>
+					<label for="cStrPos">Localizar:</label>
+					<input type="text" name="tStrPos" id="cStrPos" style="width: 130px;">
+				</fieldset>
+				<fieldset>
+					<legend>String iPOS</legend>
+					<label for="cStrIPos">Localizar:</label>
+					<input type="text" name="tStrIPos" id="cStrIPos" style="width: 130px;">
+				</fieldset>
+				<fieldset>
+					<legend>Substring Count</legend>
+					<label for="cStrCount">Contar:</label>
+					<input type="text" name="tStrCount" id="cStrCount" style="width: 130px;">
+				</fieldset>
+				<fieldset>
+					<legend>Substring</legend>
+					<label for="cSubStrt">Inicio:</label>
+					<input type="number" name="tSubStrt" id="cSubStrt" style="width: 130px;" value="<?php echo (isset($_GET['tSubStrt'])?$_GET['tSubStrt']:"-3") ?>" minlength="-29" maxlength="29">
+					<label for="cSubEnd">Fim:</label>
+					<input type="number" name="tSubEnd" id="cSubEnd" style="width: 130px;" value="<?php echo (isset($_GET['tSubEnd'])?$_GET['tSubEnd']:"3") ?>" minlength="-29" maxlength="29">
+				</fieldset>
 			</fieldset>
 			<button type="submit">Enviar</button>
 		</form>
 	</div>
 	<div class="conteudo">
-		<p class="fonte">Teste Andreia</p>
+		<p class="fonte">Aula - Funções String</p>
 		<code>
 			<?php 
-				$nome = "Andreia Hoffmann";
-				$palavras = str_word_count($nome, 0);
-				$nasc = 2000;
-				$idade = 2022 - $nasc;
-				// var_export($palavras);
-				echo "$nome tem $palavras palavras no nome.";
+			# StringToLower
+				# Altera todas letras que estiverem em maiúsculas para minúsculas
+				echo "<span style='color: white'># StringToLower</span><br>";
+
+				$stl = empty($_GET['tSTL'])?"LeonArdO":$_GET['tSTL'];
+				echo "> Recebi $stl e ficou como ".strtolower($stl);
+
+			# StringToUpper
+				# Altera todas letras que estiverem em minúsculas para maiúsculas
+				echo "<br><span style='color: white'># StringToUpper</span><br>";
+
+				$stu = empty($_GET['tSTU'])?"LeonArdO":$_GET['tSTU'];
+				echo "> Recebi $stu e ficou como ".strtoupper($stu);
+			
+			# UC First
+				# Coloca a primeira letra em maiúscula
+				echo "<br><span style='color: white'># UC First</span><br>";
+
+				$ucf = empty($_GET['tUCF'])?"leonardo borges":$_GET['tUCF'];
+				echo "> Recebi $ucf e ficou como ".ucfirst($ucf);
+
+			# UC Words
+				# Coloca a primeira letra em maiúscula de cada palavra
+				echo "<br><span style='color: white'># UC Words</span><br>";
+
+				$ucw = empty($_GET['tUCW'])?"leonardo borges":$_GET['tUCW'];
+				echo "> Recebi $ucw e ficou como ".ucwords($ucw);
+			
+			# String Reverse
+				# Reescreve a palavra ao contrário
+				echo "<br><span style='color: white'># String Reverse</span><br>";
+
+				$srev = empty($_GET['tStrRev'])?"Leonardo Borges":$_GET['tStrRev'];
+				echo "> Recebi $srev e ficou como ".strrev($srev);
+			
+			# String POS
+				# Localiza a palavra/caracter desejado e retorna sua posição[Considerando CASE]
+				echo "<br><span style='color: white'># String POS</span><br>";
+
+				$frase = "Eu vou estudar PHP";
+				$spos = empty($_GET['tStrPos'])?"PHP":$_GET['tStrPos'];
+				echo "> $frase<br>> A palavra [$spos] está na posição: ".strpos($frase, $spos);
+			
+			# String iPOS
+				# Localiza a palavra/caracter desejado e retorna sua posição[Desconsiderando CASE]
+				echo "<br><span style='color: white'># String iPOS</span><br>";
+
+				$sipos = empty($_GET['tStrIPos'])?"php":$_GET['tStrIPos'];
+				echo "> $frase<br>> A palavra [$sipos] está na posição: ".stripos($frase, $sipos);
+			
+			# Substring Count
+				# Conta quantas vezes a palavra é encontrada na frase
+				echo "<br><span style='color: white'># Substring Count</span><br>";
+
+				$frase2 = "Eu estudo PHP, eu aprendo PHP";
+				$scount = empty($_GET['tStrCount'])?"PHP":$_GET['tStrCount'];
+				echo "> $frase2<br>> A palavra [$scount] apareceu: ".substr_count($frase2, $scount)."x";
+			
+			# Substring
+				# Pega um pedaço de uma string
+				echo "<br><span style='color: white'># Substring</span><br>";
+
+				$subStrt = empty($_GET['tSubStrt'])?"-3":$_GET['tSubStrt'];
+				$subEnd = empty($_GET['tSubEnd'])?"3":$_GET['tSubEnd'];
+				echo "> $frase2<br>> Nessa posição temos: ".substr($frase2, $subStrt, $subEnd);
 			 ?>
 		</code>
 	</div>
 	<footer>
 		<button id="left"><a href="aulaphp-002.php">Voltar Página</a></button>
-		<button id="right"><a href="aulaphp-003.php">Próxima Página</a></button>
+		<button id="right"><a href="aulaphp-004.php">Próxima Página</a></button>
 	</footer>
 </body>
 </html>
---
+++ AulaPHP-004.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html>
+<head lang="pt-BR">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Testando php</title>
+</head>
+<body>
+	<header id="cabecalho">
+		<h1 class="fonte">Começando a testar comandos em PHP</h1>
+	</header>
+	<div id="formulario">
+		<form method="get" autocomplete="off">
+			<fieldset>
+				<legend>Aula - Funções String</legend>
+				<fieldset>
+					<legend>String PAD</legend>
+					<label for="cStrPad">Texto:</label>
+					<input type="text" name="tStrPad" id="cStrPad" value="<?php echo (isset($_GET['tStrPad'])?$_GET['tStrPad']:"") ?>">
+					<!-- <label for="cStrAtt">Posição:</label>
+					<select style="font-size: 10pt; width: 164px; padding: 2px" name="tStrAtt" id="cStrAtt">
+						<optgroup>
+							<option value="STR_PAD_BOTH">CENTER</option>
+							<option value="STR_PAD_RIGHT">RIGHT</option>
+							<option value="STR_PAD_LEFT">LEFT</option>
+						</optgroup>
+					</select><br> -->
+					<label>Num. de caracteres:</label>
+					<input type="number" name="tStrNum" id="cStrNum" style="width: 72px;" value="<?php echo (isset($_GET['tStrNum'])?$_GET['tStrNum']:"") ?>">
+				</fieldset>
+				<fieldset>
+					<legend>String Repeat</legend>
+					<label for="cStrRep">Texto:</label>
+					<input type="text" name="tStrRep" id="cStrRep" value="<?php echo (isset($_GET['tStrRep'])?$_GET['tStrRep']:"") ?>">
+					<label>Quantidade:</label>
+					<input type="number" name="tStrRepNum" id="cStrRepNum" style="width: 100px" value="<?php echo (isset($_GET['tStrRepNum'])?$_GET['tStrRepNum']:"") ?>">
+				</fieldset>
+				<fieldset>
+					<legend>String Replace</legend>
+					<label for="cReplace">Substituir:</label>
+					<input type="text" name="tReplace" id="cReplace" style="width:130px;" value="<?php echo (isset($_GET['tReplace'])?$_GET['tReplace']:"") ?>">
+					<label for="cReplaceBy">Por:</label>
+					<input type="text" name="tReplaceBy" id="cReplaceBy" style="width:130px;" value="<?php echo (isset($_GET['tReplaceBy'])?$_GET['tReplaceBy']:"") ?>">
+				</fieldset>
+			</fieldset>
+			<fieldset>
+				<legend>Aula - Vetores e Matrizes</legend>
+			</fieldset>
+			<button type="submit">Enviar</button>
+		</form>
+	</div>
+	<div class="conteudo">
+		<p class="fonte">Aula - Funções String</p>
+		<code>
+			<?php 
+			# String PAD
+				# Complementa os caracteres de uma string
+				echo "<br><span style='color: white'># String PAD</span><br>";
+
+				$padTxt = empty($_GET['tStrPad'])?"Leonardo":$_GET['tStrPad'];
+				// $padAtt = empty($_GET['tStrAtt'])?STR_PAD_BOTH:$_GET['tStrAtt'];
+				$padNum = empty($_GET['tStrNum'])?"30":$_GET['tStrNum'];
+				echo "> ".str_pad($padTxt, $padNum, ".", STR_PAD_BOTH);
+
+			# String Repeat
+				# Repete a string uma quantidade x de vezes
+				echo "<br><span style='color: white'># String Repeat</span><br>";
+
+				$srep = empty($_GET['tStrRep'])?"-":$_GET['tStrRep'];
+				$srepNum = empty($_GET['tStrRepNum'])?"40":$_GET['tStrRepNum'];
+				echo "> ".str_repeat($srep, $srepNum);
+			
+			# String Replace
+			# String iReplace
+				# Utilizada para substituir uma palabra por outra
+				# Quando colocado 'i' na frente do replace ele passa a desconsiderar os CASES
+				echo "<br><span style='color: white'># String Replace</span><br>";
+
+				$frase = "Eu gosto de java e faço curso de java.";
+				$replace = empty($_GET['tReplace'])?"Java":$_GET['tReplace'];
+				$replaceby = empty($_GET['tReplaceBy'])?"PHP":$_GET['tReplaceBy'];
+				echo "> A frase era:<br>$frase";
+				echo "<br>	> E a frase ficou como:<br>".str_ireplace($replace, $replaceby, $frase);
+			
+			
+			 ?>
+		</code>
+		<p class="fonte">Aula - Vetores e Matrizes</p>
+		<code>
+				<?php 
+				# Criando um vetor
+				echo "<span style='color: white'># Criando um vetor</span><br>";
+					$n = array(1,2,3);
+					$n[3] = 4;
+					$n[4] = 5;
+					$n[5] = 6;
+
+					foreach ($n as $key => $value) {
+						echo "[$key]→$value ";
+					}
+
+				# Range
+					# Alimentando meu vetor
+					echo "<br><span style='color: white'># Alimentando um vetor</span><br>";
+					$v = range(5,20,5);
+
+					foreach ($v as $key => $value) {
+						echo "[$key]→$value ";
+					}
+
+				# Unset
+					# Utilizado para desalocar uma posição do vetor, já ajustando o indice
+					echo "<br><span style='color: white'># Uso do unset</span><br>";
+					unset($n[0]);
+
+					foreach ($n as $key => $value) {
+						echo "[$key]→$value ";
+					}
+
+				# Vetor de pessoa
+					echo "<br><span style='color: white'># Vetor de pessoa</span><br>";
+					$pessoa = array("nome" => "Leonardo","idade" => 23,"sexo" => "Masculino");
+
+					foreach ($pessoa as $k => $valor) {
+						echo "[$k]→$valor ";
+					}
+
+				# Matrizes
+					echo "<br><span style='color: white'># Criando matriz</span><br>";
+					$matriz = array(array(1,2),array(3,4),array(5,6));
+					$matriz[0][0] = 7;
+					
+					foreach ($matriz as $i => $value) {
+						foreach ($value as $k => $v) {
+							echo "[$v]";
+						}
+						echo "→ Array $i<br>";
+					}
+				
+				# Array Push [Pilha / Stack]
+					echo "<span style='color: white'># Array Push</span><br>";
+					$v2 = array(1,2,3);
+					array_push($v2, 4);
+
+					foreach ($v2 as $key => $value) {
+						echo "[$key]→$value ";
+					}
+				# Array Pop [Pilha / Stack]
+					echo "<br><span style='color: white'># Array Pop</span><br>";
+					array_pop($v2);
+
+					foreach ($v2 as $key => $value) {
+						echo "[$key]→$value ";
+					}
+				# Array Unshift [Fila / ]
+					echo "<br><span style='color: white'># Array Unshift</span><br>";
+					array_unshift($v2, 10);
+
+					foreach ($v2 as $key => $value) {
+						echo "[$key]→$value ";
+					}
+				
+				# Array Shift [Fila / ]
+					echo "<br><span style='color: white'># Array Shift</span><br>";
+					array_shift($v2);
+
+					foreach ($v2 as $key => $value) {
+						echo "[$key]→$value ";
+					}
+
+				# Sort / rSort [Reverse] / aSort [Associativo] Mantendo o indices / arSort [Associativo e Reverse] / kSort [Ordenar indices] / krSort [Ordenar indices e Reverse]
+					echo "<br><span style='color: white'># Sort</span><br>";
+
+					$v3 = array(4,6,2,7,1,3);
+					foreach ($v3 as $key => $value) {
+						echo "[$key]→$value ";
+					}
+					echo "<br>";
+					sort($v3);
+					foreach ($v3 as $key => $value) {
+						echo "[$key]→$value ";
+					}
+				 ?>
+		</code>
+	</div>
+	<footer>
+		<button id="left"><a href="aulaphp-002.php">Voltar Página</a></button>
+	</footer>
+</body>
+</html>
